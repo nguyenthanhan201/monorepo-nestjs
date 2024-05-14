f793d7c68f118cc024982d76314f243e5c88e65e
nguyenthanhan201 <fxannguyen201@gmail.com>
2024-05-14 08:42:37 +0700

    ci: deploy vercel

--- apps/api/src/providers/throttler/throttler.module.ts
+++ apps/api/src/providers/throttler/throttler.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
-import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
+import { Module } from "@nestjs/common";
+import { ThrottlerModule, ThrottlerModuleOptions } from "@nestjs/throttler";
 
 const THROTTLE_CONFIG: ThrottlerModuleOptions = {
   throttlers: [
     {
-      ttl: 60000, // Time-to-live (in milliseconds) for the throttle
+      name: "medium",
+      ttl: 10000, // Time-to-live (in milliseconds) for the throttle
       limit: 20, // Maximum number of requests within the TTL period
     },
+    // {
+    //   name: "short",
+    //   ttl: 1000,
+    //   limit: 3,
+    // },
+    // {
+    //   name: "medium",
+    //   ttl: 10000,
+    //   limit: 20,
+    // },
+    // {
+    //   name: "long",
+    //   ttl: 60000,
+    //   limit: 100,
+    // },
   ],
 };
 
